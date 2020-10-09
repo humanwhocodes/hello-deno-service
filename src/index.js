7a7ef051d656fe69696202a179619469627c823c
Nicholas C. Zakas <nicholas@nczconsulting.com>
2020-10-09 08:55:49 -0700

    Fix: Ensure port is always a number

--- src/index.js
+++ src/index.js
@@ -13,14 +13,16 @@ import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
 // Setup
 //-----------------------------------------------------------------------------
 
-const PORT = Deno.env.get("PORT") || 8080;
+// port must be a number
+// https://github.com/denoland/deno/issues/7891
+const PORT = Number(Deno.env.get("PORT") ?? "8080");
 const HOST = "0.0.0.0";
 
 //-----------------------------------------------------------------------------
 // Server
 //-----------------------------------------------------------------------------
 
-const server = serve(`${ HOST }:${ PORT }`);
+const server = serve({ hostname: HOST, port: PORT });
 console.log(`HTTP webserver running.  Access it at:  http://${ HOST }:${ PORT }/`);
 
 for await (const request of server) {
