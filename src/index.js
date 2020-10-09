62a28ac4e4266385e9f0d69dda37f1fc7de0f948
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2020-10-08 19:30:03 -0700

    Fix: Remove explicit hostname

--- src/index.js
+++ src/index.js
@@ -14,13 +14,12 @@ import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
 //-----------------------------------------------------------------------------
 
 const PORT = Deno.env.get("PORT") || 8080;
-const HOST = "0.0.0.0";
 
 //-----------------------------------------------------------------------------
 // Server
 //-----------------------------------------------------------------------------
 
-const server = serve({ hostname: HOST, port: PORT });
+const server = serve({ port: PORT });
 console.log(`HTTP webserver running.  Access it at:  http://${ HOST }:${ PORT }/`);
 
 for await (const request of server) {
