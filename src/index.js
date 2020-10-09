b8d59db8aa34e87e5f9d356826c81c7f7b1fb834
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2020-10-08 19:39:32 -0700

    Fix: Try to fix server

--- src/index.js
+++ src/index.js
@@ -14,12 +14,13 @@ import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
 //-----------------------------------------------------------------------------
 
 const PORT = Deno.env.get("PORT") || 8080;
+const HOST = "0.0.0.0";
 
 //-----------------------------------------------------------------------------
 // Server
 //-----------------------------------------------------------------------------
 
-const server = serve({ port: PORT });
+const server = serve(`${ HOST }:${ PORT }`);
 console.log(`HTTP webserver running.  Access it at:  http://${ HOST }:${ PORT }/`);
 
 for await (const request of server) {
